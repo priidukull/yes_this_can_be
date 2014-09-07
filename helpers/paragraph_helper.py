9a54d74ecf3cdcbd835206119c9946f43c83e7b2
Priidu Kull <priidu.kull@gmail.com>
2014-09-07 17:07:01 +0300

    Now the section table can be filled.

--- helpers/paragraph_helper.py
+++ helpers/paragraph_helper.py
@@ -3,18 +3,18 @@ import re
 
 class Helper():
     def parse_sections(self, row):
-        paragraph = row["id"]
+        paragraph_id = row["id"]
         pg_xml = row["pg_xml"]
         sc_numbers_raw = re.findall("\\n\d\\n\(\d\)\\n\\n", pg_xml)
         sections = []
         for idx, sc_number_raw in enumerate(sc_numbers_raw):
-            section_text = pg_xml.split(sc_number_raw)[1]
+            sc_xml = pg_xml.split(sc_number_raw)[1]
             try:
-                section_text = section_text.split(sc_numbers_raw[idx+1])[0]
+                sc_xml = sc_xml.split(sc_numbers_raw[idx+1])[0]
             except IndexError:
-                pass
-            section_text = section_text.split("\n\n\n\nRT")[0]
+                sc_xml = sc_xml.split("\n\n\n<")[0]
+            sc_xml = sc_xml.split("\n\n\n\nRT")[0]
             sc_number = int(re.search("\((.*)\)", sc_number_raw).group(1))
-            sections.append({"paragraph": paragraph, "sc_number": sc_number, "text": section_text})
+            sections.append({"paragraph_id": paragraph_id, "sc_number": sc_number, "sc_xml": sc_xml})
         return sections
 
