04a14692e7dd86c8596ca1cdf04d14900c47f98c
Priidu Kull <priidu.kull@gmail.com>
2014-09-07 20:18:41 +0300

    Previously paragraphs with no numbered sections were not shown.

--- deriving/derive_sections.py
+++ deriving/derive_sections.py
@@ -16,6 +16,5 @@ class DeriveSections():
             self._section_mdl.insert_many(sections=sections)
 
 
-
 if __name__ == "__main__":
     DeriveSections().derive_all()
--- helpers/paragraph_helper.py
+++ helpers/paragraph_helper.py
@@ -13,8 +13,11 @@ class Helper():
                 sc_xml = sc_xml.split(sc_numbers_raw[idx+1])[0]
             except IndexError:
                 sc_xml = sc_xml.split("\n\n\n<")[0]
-            sc_xml = sc_xml.split("\n\n\n\nRT")[0]
+            sc_xml = sc_xml.split("\n\n\nRT")[0]
             sc_number = int(re.search("\((.*)\)", sc_number_raw).group(1))
             sections.append({"paragraph_id": paragraph_id, "sc_number": sc_number, "sc_xml": sc_xml})
+        if not sections:
+            sc_xml = pg_xml.split(row["pg_header"])[1]
+            sections.append({"paragraph_id": paragraph_id, "sc_number": 0, "sc_xml": sc_xml})
         return sections
 
